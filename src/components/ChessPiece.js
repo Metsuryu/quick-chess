d1a1cd6a7996c7c031c2e8a52897abd2d0e1bc42
Mario Cannistrà <mariocannistra.work@gmail.com>
2025-03-28 17:48:33 +0100

    Filter images

--- src/components/ChessPiece.js
+++ src/components/ChessPiece.js
@@ -3,27 +3,21 @@ import { graphql, useStaticQuery } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
 
 const ChessPiece = ({ piece }) => {
-    const data = useStaticQuery(graphql`
-      query {
-        allFile {
-          nodes {
-            relativePath
-            publicURL
-            childImageSharp {
-              gatsbyImageData(
-                width: 50
-                height: 50
-                placeholder: NONE
-                formats: [AUTO, WEBP]
-              )
-            }
+  const data = useStaticQuery(graphql`
+    {
+      allFile(filter: {extension: {eq: "png"}}) {
+        nodes {
+          relativePath
+          childImageSharp {
+            gatsbyImageData(width: 50, height: 50, placeholder: NONE, formats: [AUTO, WEBP])
           }
         }
       }
-    `);
-    
-    const key = `${piece.color}${piece.type}`;
-    const image = data?.allFile?.nodes.find(node => node.relativePath === `${key}.png`);
+    }
+  `);
+
+  const key = `${piece.color}${piece.type}`;
+  const image = data?.allFile?.nodes.find(node => node.relativePath === `${key}.png`);
 
   return <GatsbyImage image={image?.childImageSharp?.gatsbyImageData} alt={key} />;
 }
